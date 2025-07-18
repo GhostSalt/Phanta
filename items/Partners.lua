0e63d374d1b1a95c1e7f7f7fb1318a2737c2f389
GhostSalt <mhanna756@c2ken.net>
2025-07-18 23:36:24 +0100

    -Working on Catan art
    -Fixed the Ghost Partner, working on more

--- items/Partners.lua
+++ items/Partners.lua
@@ -25,7 +25,7 @@ Partner_API.Partner {
     return { vars = { card.ability.extra.xmult + benefits } }
   end,
   calculate = function(self, card, context)
-    if context.other_main and context.other_card and context.other_card.ability.set == "Tarot" then
+    if context.other_consumeable and context.other_consumeable.ability.set == "Tarot" then
       local benefits = 0
       if next(SMODS.find_card(card.ability.extra.related_card)) then benefits = 0.5 end
       return { xmult = card.ability.extra.xmult + benefits }
@@ -43,29 +43,36 @@ Partner_API.Partner {
   end
 }
 
---[[Partner_API.Partner {
+Partner_API.Partner {
   key = "cutcorners",
   name = "Cut Corners Partner",
   unlocked = false,
   discovered = true,
   pos = { x = 1, y = 0 },
   atlas = "PhantaPartners",
-  config = { extra = { related_card = "j_phanta_cutcorners",  } },
+  config = { extra = { related_card = "j_phanta_cutcorners", levels = 1, all_hands = { "High Card", "Pair", "Two Pair", "Three of a Kind", "Four of a Kind" } } },
   loc_vars = function(self, info_queue, card)
     local benefits = 0
     if next(SMODS.find_card(card.ability.extra.related_card)) then benefits = 0.5 end
-    return { vars = { card.ability.extra.xmult + benefits } }
+    return { vars = {  } }
   end,
   calculate = function(self, card, context)
-    if context.other_main and context.other_card and context.other_card.ability.set == "Tarot" then
-      local benefits = 0
-      if next(SMODS.find_card(card.ability.extra.related_card)) then benefits = 0.5 end
-      return { xmult = card.ability.extra.xmult + benefits }
+
+  end,
+  calculate_begin = function(self, card)
+    for k, v in pairs(G.GAME.hands) do
+      local counts = false
+      for _, name in ipairs(card.ability.extra.all_hands) do
+        counts = counts or k == name
+      end
+      if counts then
+        level_up_hand(card, k, true, card.ability.extra.levels)
+      end
     end
   end,
   check_for_unlock = function(self, args)
     for _, v in pairs(G.P_CENTER_POOLS["Joker"]) do
-      if v.key == "j_phanta_ghost" then
+      if v.key == "j_phanta_cutcorners" then
         if get_joker_win_sticker(v, true) >= 8 then
           return true
         end
@@ -73,4 +80,4 @@ Partner_API.Partner {
       end
     end
   end
-}]]--
+}
