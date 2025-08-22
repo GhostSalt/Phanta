6f64507af5a464582f3bf4206a7ad912d24fb258
GhostSalt <mhanna756@c2ken.net>
2025-08-22 09:55:35 +0100

    -Aiko's and Entropy are now compatible

--- main.lua
+++ main.lua
@@ -245,8 +245,7 @@ function count_missing_ranks()
   end
 
   if G.GAME and G.GAME.phanta_initial_ranks and #existing_ranks < #G.GAME.phanta_initial_ranks then
-    return #G.GAME
-    .phanta_initial_ranks - #existing_ranks
+    return #G.GAME.phanta_initial_ranks - #existing_ranks
   end
   return 0
 end
@@ -464,10 +463,13 @@ function SMODS.current_mod.reset_game_globals(run_start)
   local value_table = { '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace' }
   G.GAME.current_round.train_station_card.value = value_table[G.GAME.current_round.train_station_card.id - 1]
 
+  if not G.GAME.current_round.puzzle_card then G.GAME.current_round.puzzle_card = { id = 2 } end
+
   if (get_previous_blind() and get_previous_blind() == "boss") or not G.GAME.last_blind then
     if G.playing_cards then
       G.GAME.current_round.puzzle_card.id = pseudorandom_element(G.playing_cards,
         pseudoseed('puzzle' .. G.GAME.round_resets.ante)):get_id()
+      if not G.GAME.current_round.puzzle_card.id then G.GAME.current_round.puzzle_card.id = 2 end
     else
       G.GAME.current_round.puzzle_card.id = 2
     end
